a37399a04cb89bbc4248fc9de24f3c13aacffef7
leloeduk <leloeduk2025@gmail.com>
2026-01-07 01:09:23 +0100

    fix(todo): update TodoModel

--- lib/features/todo/data/models/todo_model.dart
+++ lib/features/todo/data/models/todo_model.dart
@@ -8,7 +8,8 @@ class TodoModel {
 
   TodoModel({required this.id, required this.title, required this.done});
 
-  factory TodoModel.fromEntity(Todo t) => TodoModel(id: t.id, title: t.title, done: t.done);
+  factory TodoModel.fromEntity(Todo t) =>
+      TodoModel(id: t.id, title: t.title, done: t.done);
   Todo toEntity() => Todo(id: id, title: title, done: done);
 }
 
