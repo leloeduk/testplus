aa82749397ecbfa70aec95f8febfa78e25fc2abc
leloeduk <leloeduk2025@gmail.com>
2026-01-07 01:24:56 +0100

    chore(todo): update lib/pages/domain/repositories/expense_repos.dart

--- lib/pages/domain/repositories/expense_repos.dart
+++
@@ -1,8 +0,0 @@
-import 'package:lelochat/pages/domain/entities/expense.dart';
-
-abstract class ExpenseRepository {
-  List<Expense> getExpenses();
-  void addExpense(Expense expense);
-  void deleteExpense(int index);
-  double getTotal();
-}
